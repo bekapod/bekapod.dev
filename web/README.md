df95742ef87803793c4ce7181fa155b642798466
Sanity.io <hello@sanity.io>
2021-05-09 12:46:35 +0100

    Initial commit

---
+++ web/README.md
@@ -0,0 +1,1 @@
+# bekapod.dev-web
---
+++ web/client-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  sanity: {
+    projectId: process.env.SANITY_PROJECT_ID || 'lzxc2amy',
+    dataset: process.env.SANITY_DATASET || 'production'
+  }
+}
